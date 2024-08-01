ec97914fad88b37c9562432c931a65c32f47b833
luishenriqueom <luis.henryke1@gmail.com>
2024-08-01 01:51:45 -0300

    feat(deposit, transfer): implementado funcoes de deposito e transferencia, alem dos testes de cada funcao

--- test_account.py
+++ test_account.py
@@ -4,6 +4,7 @@ from account import Account
 class TestAccount(unittest.TestCase):
     def setUp(self):
         self.account = Account(initial_balance=100)
+        self.account2 = Account(initial_balance=0)
     
     def test_withdraw_success(self):
         self.account.withdraw(50)
@@ -16,6 +17,27 @@ class TestAccount(unittest.TestCase):
     def test_withdraw_negative_amount(self):
         with self.assertRaises(ValueError):
             self.account.withdraw(-10)
+
+    def test_deposit_success(self):
+        self.account.deposit(50)
+        self.assertEqual(self.account.balance, 150)
+
+    def test_deposit_negative_amount(self):
+        with self.assertRaises(ValueError):
+            self.account.deposit(-20)
+
+    def test_transferMoney_success(self):
+        self.account.transferAmount(100, self.account2)
+        self.assertEqual(self.account.balance, 0)
+        self.assertEqual(self.account2.balance, 100)
+
+    def test_transferMoney_insufficient_funds(self):
+        with self.assertRaises(ValueError):
+            self.account.transferAmount(200, self.account2)
+    
+    def test_transferMoney_negative_amount(self):
+        with self.assertRaises(ValueError):
+            self.account.transferAmount(-20, self.account2)
 
 if __name__ == '__main__':
     unittest.main()
